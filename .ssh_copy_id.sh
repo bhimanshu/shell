27501204401bae25b21dd19f877f5e03b6ddb165
bhimanshu <bhimanshu@juniper.net>
2018-12-04 00:09:57 -0800

    Update .ssh_copy_id.sh

--- .ssh_copy_id.sh
+++ .ssh_copy_id.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 #usage ./ssh_copy_id.sh user_name@IP... eg ./ssh_copy_id.sh root@10.0.0.50
 #Also make sure expect is installed. you may use apt-get install python-pip and then pip install expect on ubuntu systems
-#which expect should output the path 
+#"which expect" should output the path 
 set timeout 3
 set ip [lindex $argv 0];
 spawn ssh-copy-id "$ip"
